80f492f578b96a9a4cd1165d82df78b75686d9f5
root <admin@example.com>
2020-01-13 17:40:28 +0200

    Implemented official mirror check

--- verify.js
+++ verify.js
@@ -4,7 +4,6 @@ function postAjax(url, data, success) {
 	).join('&');
 	var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
 	xhr.open('POST', url);
-//	xhr.setRequestHeader('X-CSRF-TOKEN', $('meta[name="_token"]').attr('content'));
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState>3 && xhr.status==200) { success(xhr.responseText); }
 		if (xhr.readyState>3 && xhr.status!==200) { success(xhr.responseText); }
@@ -18,8 +17,16 @@ function postAjax(url, data, success) {
 host = window.location.hostname;
 postAjax('https://onion.live/api/store/reports/search', 'url$re='+host+'/&type=2000349', function(data){
 	obj = JSON.parse(data);
+//	console.log(obj);
 	if(obj.total > 0) {
 		document.body.innerHTML += '<div style="position:absolute;width:100%;background: #000; text-align: center;z-index:100;top: 0px;"><h1 style="color: red">This site has '+obj.total+' phishing reports</h1></div>';
 	}
 });
 
+postAjax('https://onion.live/api/store/mirrors/search', 'url=http://'+host+'&page=1&perpage=1', function(data){
+	obj = JSON.parse(data);
+	console.log(obj);
+	if(obj.total > 0) {
+		document.body.innerHTML += '<div style="position:absolute;width:100%;background: #000; text-align: center;z-index:100;top: 0px;"><h1 style="color: red">This is an official mirror</h1></div>';
+	}
+})
