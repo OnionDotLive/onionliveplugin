a2c74c3fa6385dc06403f2b29f6b1a958e296acc
root <admin@example.com>
2020-01-13 20:05:08 +0200

    Updating logo for opened tab only

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 'use strict';
 function handleMessage(request, sender, sendResponse) {
   browser.browserAction.setPopup({popup: 'popup/popup.html'})
-//  browser.browserAction.enable();
   browser.browserAction.openPopup();
   console.log("Message from the content script: " + request.message);
   sendResponse({response: "Response from background script"});
+  browser.browserAction.setIcon({tabId: sender.tab.id, path: "icons/"+request.icon+".png"});
   browser.notifications.create({
 	"type": "basic",
-	"iconUrl": browser.extension.getURL("icons/logo.png"),
+	"iconUrl": browser.extension.getURL("icons/"+request.icon+".png"),
 	"title": request.title,
 	"message": request.message
   });
