e9736e9f361c73a28b58e42014d126ebf26201a6
root <admin@example.com>
2020-01-15 21:36:10 +0200

    Phishing Mirrors are now checked for site and suggesting an alternative working mirror

--- verify.js
+++ verify.js
@@ -1,12 +1,29 @@
 //'use strict';
 
+function appendMirr(data) {
+console.log(data);
+	mirr = JSON.parse(data).data[0];
+	if(mirr.url) {
+		var div = document.createElement("h1");
+		div = '<br/><a style="background: #2d2d2d; color:yellow; text-align: center" href="'+mirr.url+'">Try this working mirror instead</a>';
+		var element = document.getElementById('mainwarn');
+		element.innerHTML += div;
+	}
+}
+
+function newMirr(site, success) {
+	if(site) {
+		postAjax('https://onion.live/api/store/mirrors/search', 'site='+site+"&ctime$ne=60&orderby=official&orderas=DESC&page=1&perpage=1", success);
+	}
+}
+
 
 function handleResponse(message) {
-  console.log(`Message from the background script:  ${message.response}`);
+	console.log(`Message from the background script:  ${message.response}`);
 }
 
 function handleError(error) {
-  console.log(`Error: ${error}`);
+	console.log(`Error: ${error}`);
 }
 
 function postAjax(url, data, success) {
@@ -26,30 +43,40 @@ function postAjax(url, data, success) {
 }
 
 host = window.location.hostname;
-
-// Checking for phishing
-postAjax('https://onion.live/api/store/reports/search', 'url$re='+host+'/&type=2000349', function(data){
-	obj = JSON.parse(data);
-	if(obj.total > 0) {
-		var sending = browser.runtime.sendMessage({title: "Phishing Mirror", message: "URL: "+host+" has been reported as a phishing mirror.", type: "phishing", icon: "error", host: host});
-		const p = document.createElement("h1");
-		p.textContent = "WARNING: Phishing Mirror !!!";
-		p.setAttribute("style", "background: #2d2d2d; color:red; font-size:4em; text-align: center");
-		document.body.insertBefore(p, document.body.firstChild);
-		sending.then(handleResponse, handleError);
-	}
-});
-
-// Checking if mirror is listed in our DB
-postAjax('https://onion.live/api/store/mirrors/search', 'url=http://'+host+'&page=1&perpage=1', function(data){
-	obj = JSON.parse(data);
-	if(obj.total > 0) {
-		if(obj.data[0].official !== "1") {
-			var sending = browser.runtime.sendMessage({title: "Trusted Mirror", message: "URL: "+host+" is listed on our directory.", type: "trusted", icon: "verified", host: host});
-		} else {
-			var sending = browser.runtime.sendMessage({title: "Trusted Mirror", message: "URL: "+host+" is marked official in our directory.", type: "trusted", icon: "official", host: host});
+if(host) {
+	// Checking for phishing
+	postAjax('https://onion.live/api/store/reports/search', 'url$re='+host+'/&type=2000349', function(data){
+		obj = JSON.parse(data);
+		if(obj.total > 0) {
+			if(obj.data[0].site.id) {
+				site = obj.data[0].site;
+				newMirr(site.id, appendMirr);
+				var sending = browser.runtime.sendMessage({title: "Phishing Mirror", message: "URL: "+host+" has been reported as a phishing mirror of "+site.name+".", type: "phishing", icon: "error", host: host});
+			} else {
+				var sending = browser.runtime.sendMessage({title: "Phishing Mirror", message: "URL: "+host+" has been reported as a phishing mirror.", type: "phishing", icon: "error", host: host});
+			}
+			const h1 = document.createElement("h1");
+			h1.textContent = "WARNING: Phishing Mirror !!!";
+			h1.setAttribute("style", "background: #2d2d2d; color:red; font-size:4em; text-align: center");
+			h1.setAttribute("id", "mainwarn");
+			document.body.insertBefore(h1, document.body.firstChild);
+			sending.then(handleResponse, handleError);
 		}
-		sending.then(handleResponse, handleError);
-	}
-})
+	});
 
+	// Checking if mirror is listed in our DB
+	postAjax('https://onion.live/api/store/mirrors/search', 'url=http://'+host+'&page=1&perpage=1', function(data){
+		obj = JSON.parse(data);
+		if(obj.total > 0) {
+			if(obj.data[0].site.id) {
+				site = obj.data[0].site;
+			}
+			if(obj.data[0].official !== "1") {
+				var sending = browser.runtime.sendMessage({title: "Trusted Mirror", message: "URL: "+host+" is listed on our directory.", type: "trusted", icon: "verified", host: host});
+			} else {
+				var sending = browser.runtime.sendMessage({title: "Trusted Mirror", message: "URL: "+host+" is marked official in our directory.", type: "trusted", icon: "official", host: host});
+			}
+			sending.then(handleResponse, handleError);
+		}
+	});
+}
