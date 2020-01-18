ec0fa168e6f8202de8a5607f93e8e720a611bcf5
root <admin@example.com>
2020-01-18 22:08:48 +0200

    - Implemented popup url check. - Implemented popup sites/mirrors browser. - Enhanced the styling a bit to match onion.live's darktheme. - Limited all results in popup to only 10 to allow a better UX. - Updated the extension zip.

--- popup/popup.js
+++ popup/popup.js
@@ -30,22 +30,97 @@ function getAjax(url, data, success) {
 	return xhr;
 }
 
-window.showsite = function(site) {
-	document.querySelector('.mirrs').innerHTML = '<table class="restbl"><thead><tr><td>URL</td><td>Uptime</td><td>Connection Time</td></tr></thead><tbody class="mirrbdy"></tbody></table>';
-	postAjax('https://onion.live/api/store/mirrors/search', 'site='+site+"&ctime$ne=60&orderby=official&orderas=DESC&page=1&perpage=all", function(data){
+function appendMirr(data) {
+	mirr = JSON.parse(data).data[0];
+	if(mirr.url) {
+		var div = document.createElement("h1");
+		div = '<br/><a style="background: #2d2d2d; color:yellow; text-align: center" href="'+mirr.url+'">Try this working mirror instead</a>';
+		var element = document.getElementById('mainwarn');
+		element.innerHTML += div;
+	}
+}
+
+function newMirr(site, success) {
+	if(site) {
+		postAjax('https://onion.live/api/store/mirrors/search', 'site='+site+"&ctime$ne=60&orderby=official&orderas=DESC&page=1&perpage=1", success);
+	}
+}
+
+window.verify = function(host) {
+	document.querySelector('.note-container').style.display = "inline-block";
+	// Checking for phishing
+	postAjax('https://onion.live/api/store/reports/search', 'url$re='+host+'/&type=2000349', function(data){
 		obj = JSON.parse(data);
-		for(i=0;i<obj.total;i++) {
-			document.querySelector('.mirrbdy').innerHTML += '<tr><td><a href="'+obj.data[i].url+'">'+obj.data[i].url+'</a></td><td>'+obj.data[i].uptime+'%</td><td>'+obj.data[i].ctime+'s</td></tr>';
+		if(obj.total > 0) {
+			document.querySelector('.mainresults').innerHTML += '<img class="statuslogo" src="error.png" width="100"/><br/><span class="statuswarn status">Phishing Mirror !!!</span>';
+			if(obj.data[0].site.id) {
+				site = obj.data[0].site;
+				document.querySelector('.mainresults').innerHTML += '<br/><span class="statuswarn status">'+site.name+'</span>';
+				showsite(site.id);
+			}
+		}
+		var input = document.querySelector('.search');
+		input.addEventListener('keyup', (event) => {
+			searchSites(event.target.value);
+		});
+	});
+
+	// Checking if mirror is listed in our DB
+	postAjax('https://onion.live/api/store/mirrors/search', 'url=http://'+host+'&url$eq$or=https://'+host+'&url$is$or=http://'+host+'/&url$as$or=https://'+host+'/&page=1&perpage=1', function(data){
+		obj = JSON.parse(data);
+		if(obj.total > 0) {
+			if(obj.data[0].official !== "1") {
+				document.querySelector('.mainresults').innerHTML += '<img class="statuslogo" src="verified.png" width="100"/><br/><span class="statusok status">Trusted Mirror !!!</span>';
+			} else {
+				document.querySelector('.mainresults').innerHTML += '<img class="statuslogo" src="official.png" width="100"/><br/><span class="statusok status">Official Mirror !!!</span>';
+			}
+			if(obj.data[0].site.id) {
+				site = obj.data[0].site;
+				document.querySelector('.mainresults').innerHTML += '<br/><span class="statusok status">'+site.name+'</span>';
+				showsite(site.id);
+			}
+		}
+		document.querySelector('.note-container').style.display = "none";
+		var input = document.querySelector('.search');
+		input.addEventListener('keyup', (event) => {
+			searchSites(event.target.value);
+		});
+	});
+};
+
+window.showsite = function(site) {
+	document.querySelector('.note-container').style.display = "inline-block";
+	document.querySelector('.mirrs').innerHTML = '<table class="restbl"><thead><tr><td>URL</td><td>Uptime</td><td>Connection Time</td></tr></thead><tbody class="mirrbdy"></tbody></table>';
+	postAjax('https://onion.live/api/store/mirrors/search', 'site='+site+"&ctime$ne=60&orderby=official&orderas=DESC&page=1&perpage=10", function(data){
+		document.querySelector('.note-container').style.display = "none";
+		obj = JSON.parse(data);
+		for(i=0;i<obj.data.length;i++) {
+			document.querySelector('.mirrbdy').innerHTML += '<tr><td class="urltd"><a href="'+obj.data[i].url+'">'+obj.data[i].url+'</a></td><td>'+obj.data[i].uptime+'%</td><td>'+obj.data[i].ctime+'s</td></tr>';
 		}
 	});
 };
 
-window.searchSites = function(search) {
-	postAjax('https://onion.live/api/store/sites/search', 'name$re='+search+"&description$re$or="+search+"&page=1&perpage=all", function(data){
+window.searchSites = function(search = null) {
+	document.querySelector('.note-container').style.display = "inline-block";
+	if(search) {
+		query = 'name$re='+search+'&description$re$or='+search+'&page=1&perpage=10&orderby=featured&orderas=DESC';
+	} else {
+		query = 'featured=1&page=1&perpage=10';
+	}
+	postAjax('https://onion.live/api/store/sites/search', query, function(data){
+		logo = document.querySelector('.statuslogo');
+		if(logo) {
+			logo.parentNode.removeChild(logo);
+		}
+		warn = document.querySelector('.status');
+		if(warn) {
+			warn.parentNode.removeChild(warn);
+		}
+		document.querySelector('.note-container').style.display = "none";
 		var inputTitle = document.querySelector('.results');
 		inputTitle.innerHTML = '<ul class="resul"></ul><div class="mirrs"></div>';
 		obj = JSON.parse(data);
-		for(i=0;i<obj.total;i++) {
+		for(i=0;i<obj.data.length;i++) {
 			document.querySelector('.resul').innerHTML += "<li><a href='#' id='"+obj.data[i].id+"' class='site'>"+obj.data[i].name+"</a></li>";
 		}
 		for(const k of document.querySelectorAll('.site')) {
@@ -62,17 +137,19 @@ function getCurrentTab() {
 		if(url && url != 'about:blank') {
 			handleURL(url);
 		} else {
-			dispList();
+			dispList(1);
 		}
 	})
 }
 
-function dispList(){
+function dispList(featured){
 	var inputTitle = document.querySelector('.content');
-	inputTitle.innerHTML = '<input type="text" class="search" name="search" placeholder="Search for site">';
 	inputTitle.innerHTML += '<div class="results"><div>';
+	if(featured === 1) {
+		searchSites();
+	}
 	var input = document.querySelector('.search');
-	input.addEventListener('change', (event) => {
+	input.addEventListener('keyup', (event) => {
 		searchSites(event.target.value);
 	});
 }
@@ -80,6 +157,12 @@ function dispList(){
 function handleURL(url){
 	var inputTitle = document.querySelector('.url');
 	inputTitle.value = url;
+	var parser = document.createElement('a');
+	parser.href = url;
+	dispList(0);
+	var results = document.querySelector('.results');
+	results.innerHTML = '<ul class="resul"></ul><div class="mirrs"></div>';
+	verify(parser.hostname);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
